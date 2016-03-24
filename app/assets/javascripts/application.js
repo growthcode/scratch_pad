606fa6b4c6af59090a4890ea5a61630d8d59faf8
growthcode <growthcode@gmail.com>
2016-03-24 00:18:58 -0700

    add backbone router

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require lodash
+//= require underscore
+// require lodash
 //= require backbone
 //= require scratch_pad
 //= require_tree ../templates
